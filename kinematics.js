972f6449fb1ae02550fec1cd5a2fe0e0c9f03097
Jake Bridge <jakebridge2005@gmail.com>
2019-12-03 18:49:01 -0700

    Began eq2. Needs more work

--- kinematics.js
+++ kinematics.js
@@ -24,6 +24,10 @@ function bringUpForm(equation)  {
 
     }else if (equation == 'eq2') {
         document.forms.equation2.classList.remove('hidden')
+        document.forms.equation2.onchange = function() {
+            console.log(this.elements.vf.value)
+            eq2answer.innerHTML = solveEquation2(this.elements.vf.value, this.elements.a.value, this.elements.delx.value, this.elements.vi.value)
+        }
 
     }else if (equation == 'eq3') {
         document.forms.equation3.classList.remove('hidden')
@@ -31,8 +35,54 @@ function bringUpForm(equation)  {
     };
 
 };
+function solveEquation2(vf, a, delx, vi) { //Should be working.... TODO: Further testing
+    let valarray = [vf, a, delx, vi];
+    let nulls = 0;
+    for (let index = 0;index < valarray.length; index++) {
+        const element = valarray[index]
+        if (element == '') {
+            valarray[index] = null
+            nulls++
+        }else {
+            valarray[index] = parseFloat(element)
+        }
+    }
+    vf = valarray[0]
+    a = valarray[1]
+    delx = valarray[2]
+    vi = valarray[3]
+    if (nulls == 1) {
+        try {
+            if (vf == null) {
+                // vf = sqrt(2a * delx + vi^2)
+                return `Final Velocity: ${(Math.sqrt(2*a * delx + (vi * vi)))}`
+            }else if (a == null) {
+                // vf = sqrt(2a * delx + vi^2)
+                // vf^2 = 2a * delx + vi^2
+                // (vf^2 - vi^2)/(2 * delx) = a
+                return `Acceleration: ${(vf * vf - (vi * vi))/(2 * delx)}`
+            }else if (delx == null) {
+                // vf = sqrt(2a * delx + vi^2)
+                // vf^2 = 2a + vi^2 * delx
+                // delx = (vf * vf - vi^2)/(2 * a)
+                return `Change in X: ${(((vf * vf)-(vi * vi))/(2 * a))}`
+
+            }else if (vi == null) {
+                //vf^2 = 2a*delx + vi^2
+                //vf^2-2adelx = vi^2
+                return `Initial Velocity: ${Math.sqrt((vf * vf)-(2 * a *delx))}`
+            }
+            
+        } catch (error) {
+            return `Something went wrong. Please re-enter your values. (Error:${error})`            
+        }
+    }else {
+        return 'Not enough values. (Or too many). Make sure you have 3.'
+    }
+
+};
 function solveEquation1(v, a, t, v0) {
-    valarray = [v, a, t, v0]
+    let valarray = [v, a, t, v0]
     let nulls = 0;
     for (let index = 0; index < valarray.length; index++) {
         const element = valarray[index];
@@ -66,17 +116,17 @@ function solveEquation1(v, a, t, v0) {
         try {
             if (v == null) {
                 // v = at + v0
-                return `v = ${a * t + v0}`;
+                return `Final Velocity: ${a * t + v0}`;
             }else if (a == null) {
                 // v = at + v0
                 // v - v0 = at
                 // (v-v0)/t = a
-                return `a = ${(v-v0)/t}`
+                return `Acceleration: ${(v-v0)/t}`
             }else if (t == null) {
                 //v = at + v0
                 //(v-v0) = at
                 // (v-v0)/a = t
-                return `t = ${(v-v0)/a}`
+                return `Time: ${(v-v0)/a}`
             }else if (v0 == null) {
                 //v = at + v0
                 // v- at = v0
@@ -84,7 +134,7 @@ function solveEquation1(v, a, t, v0) {
             }
             
         } catch (error) {
-            return `Something went wrong. Please re-enter your values. (Error:${error}`
+            return `Something went wrong. Please re-enter your values. (Error:${error})`
         }
 
     }else {
